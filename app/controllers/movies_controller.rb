03a078434f7bba4d2373443b5df4d9be8077ffa6
EC2 Default User <ec2-user@ip-172-31-32-146.us-east-2.compute.internal>
2021-09-23 09:15:23 +0000

    part 1 finished

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,8 @@ class MoviesController < ApplicationController
     end
   
     def index
-      @movies = Movie.all
+      @movies = Movie.order(params[:sortTerm])
+      @selected = params[:sortTerm]
     end
   
     def new
